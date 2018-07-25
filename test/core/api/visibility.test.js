dad6f77788995b71ba8449cb71b82b6ae8c2d382
Guy Brill <brill.guy1@gmail.com>
2018-07-25 19:32:57 +0300

    visibility tests

---
+++ test/core/api/visibility.test.js
@@ -0,0 +1,37 @@
+const visibility = require('../../../src/core/api/visibility');
+import {vffData} from '../../../src/core/vffData.js';
+
+let data = {visibility: false};
+
+describe("Visibility", () =>{
+
+   describe("Show", () => {
+      it("Should show the graphics of the passed template, set visibility to true", () => {
+          let template = vffData.registerTemplate('test', data);
+          expect(template.visibility).toBeFalsy();
+          visibility.show('test');
+          expect(template.visibility).toBeTruthy();
+      });
+   });
+   describe("Hide", () => {
+        it("Should hide the graphics of the passed template, set visibility to false", () => {
+            let template = vffData.registerTemplate('test', data);
+            expect(template.visibility).toBeFalsy();
+            visibility.show('test');
+            expect(template.visibility).toBeTruthy();
+            visibility.hide('test');
+            expect(template.visibility).toBeFalsy();
+        });
+    });
+    describe("Toggle", () => {
+        it("Should hide or show the graphics of the passed template based on current visibility state", () => {
+            let template = vffData.registerTemplate('test', data);
+            expect(template.visibility).toBeFalsy();
+            visibility.toggle('test');
+            expect(template.visibility).toBeTruthy();
+            visibility.toggle('test');
+            expect(template.visibility).toBeFalsy();
+        });
+    });
+
+});
