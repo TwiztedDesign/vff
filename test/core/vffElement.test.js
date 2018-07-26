63ba99a020bee2bf1d145c1309236b72d4d94ba7
Guy Brill <brill.guy1@gmail.com>
2018-07-26 12:49:21 +0300

    vffElement tests, new element from html tag

--- test/core/vffElement.test.js
+++ test/core/vffElement.test.js
@@ -1,20 +1,28 @@
 import VffElement from '../../src/core/vffElement';
+// import {init} from '../../src/core/vffElement';
+//
+// jest.mock('../../src/core/vffElement');
 
 describe('VFF Element', () => {
+    // beforeEach(() =>{
+    //     init.mockClear();
+    // });
 
     describe('New Element', () => {
-
         it('Should initialize the vff element', () => {
-
             let headerElement = document.createElement('h1');
             headerElement.setAttribute("vff-template", 'new template');
             headerElement.setAttribute("vff-name", 'title');
             document.body.appendChild(headerElement);
 
             let vffElementCreated = new VffElement('h1');
+            // expect(init).toHaveBeenCalledTimes(1);
             expect(vffElementCreated.element).toEqual(headerElement);
-            // let vffElementCreated = new VffElement('<h1>');
-            // expect(vffElementCreated.element).toEqual(headerElement);
+        });
+        it('Should initialize and create the vff element', () => {
+            let vffElementCreated = new VffElement('<h2>');
+            let headerElement = document.createElement('h2');
+            expect(vffElementCreated.element).toEqual(headerElement);
         });
 
         it('Should throw error, Invalid Node', () => {
