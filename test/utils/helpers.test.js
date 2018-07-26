94984635538e0c9891d2b0e2a30e922eaecf5ce4
dok <dumansky@gmail.com>
2018-07-26 17:01:54 +0300

    tests

--- test/utils/helpers.test.js
+++ test/utils/helpers.test.js
@@ -190,12 +190,15 @@ describe('Helpers', () => {
         it('should work', () => {
             expect(helpers.deepCompare({},{})).toBeTruthy();
             expect(helpers.deepCompare({a:1}, {a:1})).toBeTruthy();
+            expect(helpers.deepCompare({a:[1,2]}, {a:[1,2]})).toBeTruthy();
             expect(helpers.deepCompare({a:1}, new Proxy({a:1},{}))).toBeTruthy();
             expect(helpers.deepCompare({a:{b:{c:1}}},{a:{b:{c:1}}})).toBeTruthy();
             expect(helpers.deepCompare({a:1},{a:2})).toBeFalsy();
             expect(helpers.deepCompare({a:1},{b:1})).toBeFalsy();
             expect(helpers.deepCompare({a:1},{a:1, b:1})).toBeFalsy();
             expect(helpers.deepCompare({a:1},{a:"1"})).toBeFalsy();
+            expect(helpers.deepCompare({a:[1,2]},{a:[1,3]})).toBeFalsy();
+            expect(helpers.deepCompare({a:{b:[1,2]}},{a:{b:[1,2]}})).toBeTruthy();
         });
     });
     describe('noop', () => {
