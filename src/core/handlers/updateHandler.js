d1cd9574202b984484058bfd735a42a8c2b6be5d
dok <dumansky@gmail.com>
2018-09-06 13:49:21 +0300

    handle mouse events

--- src/core/handlers/updateHandler.js
+++ src/core/handlers/updateHandler.js
@@ -20,10 +20,32 @@ function update(data){
             });
             promises.push(deferred.promise);
         }
+        // if(['mousemove', 'mousedown', 'mouseup','click'].indexOf(templateName) ){
+        if(['click'].indexOf(templateName) > -1 ){
+            var target = lookupElementByXPath(data[templateName].target);
+            // data[templateName].target = target;
+            data[templateName].bubbles = true;
+            data[templateName].detail = {vff: 'vff'};
+            data[templateName].ctrlKey = true;
+            // if(vff.mode === 'normal' || vff.mode === 'controller-program') {
+                target.dispatchEvent(new MouseEvent(templateName, data[templateName]));
+                // target.dispatchEvent(new CustomEvent(templateName, {bubble: true, detail : data[templateName]}));
+            // }
+        }
     }
 
     document.dispatchEvent(new CustomEvent(VFF_EVENT, { detail: data }));
     return Promise.all(promises);
+}
+
+function lookupElementByXPath(path) {
+    var evaluator = new XPathEvaluator();
+    var result = evaluator.evaluate(path, document.documentElement, null,XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+    return  result.singleNodeValue;
+}
+
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
 function updateDom(template, control, value, timecode){
--- src/core/interactionEvents.js
+++ src/core/interactionEvents.js
@@ -19,6 +19,10 @@ function touchesToJson(touches){
 }
 
 function bubbleUpMouseEvent(e){
+    if(e.ctrlKey){
+        console.log('with ctrl');
+        return;  
+    } 
     send(BUBBLE_UP,{
         event : e.type,
         data  : {
@@ -26,12 +30,46 @@ function bubbleUpMouseEvent(e){
             pageY : e.pageY,
             clientX : e.clientX,
             clientY : e.clientY,
+            target : createXPathFromElement(e.target),
             touches : touchesToJson(e.touches),
             targetTouches : touchesToJson(e.targetTouches),
             changedTouches : touchesToJson(e.changedTouches)
+
         }
     });
 }
+
+
+
+
+function createXPathFromElement(elm) {
+    var allNodes = document.getElementsByTagName('*');
+    for (var segs = []; elm && elm.nodeType == 1; elm = elm.parentNode)
+    {
+        if (elm.hasAttribute('id')) {
+            var uniqueIdCount = 0;
+            for (var n=0;n < allNodes.length;n++) {
+                if (allNodes[n].hasAttribute('id') && allNodes[n].id == elm.id) uniqueIdCount++;
+                if (uniqueIdCount > 1) break;
+            };
+            if ( uniqueIdCount == 1) {
+                segs.unshift('id("' + elm.getAttribute('id') + '")');
+                return segs.join('/');
+            } else {
+                segs.unshift(elm.localName.toLowerCase() + '[@id="' + elm.getAttribute('id') + '"]');
+            }
+        } else if (elm.hasAttribute('class')) {
+            segs.unshift(elm.localName.toLowerCase() + '[@class="' + elm.getAttribute('class') + '"]');
+        } else {
+            for (var i = 1, sib = elm.previousSibling; sib; sib = sib.previousSibling) {
+                if (sib.localName == elm.localName)  i++; };
+            segs.unshift(elm.localName.toLowerCase() + '[' + i + ']');
+        };
+    };
+    return segs.length ? '/' + segs.join('/') : null;
+};
+
+
 
 
 function onTouchStart(e){
@@ -73,6 +111,7 @@ window.addEventListener('load', () => {
     document.body.addEventListener('mousemove', bubbleUpMouseEvent);
     document.body.addEventListener("mousedown", bubbleUpMouseEvent);
     document.body.addEventListener("mouseup", bubbleUpMouseEvent);
+    document.body.addEventListener("click", bubbleUpMouseEvent);
 
 
 });
