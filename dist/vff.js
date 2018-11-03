e70aa643f210a6f609130f157d59d6f7eb707194
dok <dumansky@gmail.com>
2018-11-04 00:06:13 +0200

    handle cursor query param

--- dist/vff.js
+++ dist/vff.js
@@ -2113,7 +2113,22 @@ function queryParams(data) {
     _vffData.vffData.addQueryParams(data);
 
     if (data && data.cursor && data.cursor === '0') {
-        document.styleSheets[0].addRule('*', 'cursor: none !important');
+
+        // document.styleSheets[0].addRule('*', 'cursor: none !important');
+
+        var css = '* { cursor: none !important; }',
+            head = document.head || document.getElementsByTagName('head')[0],
+            style = document.createElement('style');
+
+        style.type = 'text/css';
+        if (style.styleSheet) {
+            // This is required for IE8 and below.
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
+
+        head.appendChild(style);
     }
 }
 
