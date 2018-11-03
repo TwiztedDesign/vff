2fb38de239778a981da0d75da07ae963cd6bfe8d
dok <dumansky@gmail.com>
2018-11-04 00:06:01 +0200

    handle cursor query param

--- src/core/handlers/queryParamsHandler.js
+++ src/core/handlers/queryParamsHandler.js
@@ -4,7 +4,22 @@ function queryParams(data) {
     vffData.addQueryParams(data);
 
     if(data && data.cursor && data.cursor === '0'){
-        document.styleSheets[0].addRule('*', 'cursor: none !important');
+
+        // document.styleSheets[0].addRule('*', 'cursor: none !important');
+
+        var css = '* { cursor: none !important; }',
+            head = document.head || document.getElementsByTagName('head')[0],
+            style = document.createElement('style');
+
+        style.type = 'text/css';
+        if (style.styleSheet){
+            // This is required for IE8 and below.
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
+
+        head.appendChild(style);
     }
 }
 
