bb1285c13b2a0c7eb471b3127a4ed36c51164024
dok <dumansky@gmail.com>
2018-11-05 18:30:41 +0200

    handle cursor query param (revert)

--- dist/vff.js
+++ dist/vff.js
@@ -2111,25 +2111,6 @@ var _vffData = __webpack_require__(2);
 
 function queryParams(data) {
     _vffData.vffData.addQueryParams(data);
-
-    if (data && data.cursor && data.cursor === '0') {
-
-        // document.styleSheets[0].addRule('*', 'cursor: none !important');
-
-        var css = '* { cursor: none !important; }',
-            head = document.head || document.getElementsByTagName('head')[0],
-            style = document.createElement('style');
-
-        style.type = 'text/css';
-        if (style.styleSheet) {
-            // This is required for IE8 and below.
-            style.styleSheet.cssText = css;
-        } else {
-            style.appendChild(document.createTextNode(css));
-        }
-
-        head.appendChild(style);
-    }
 }
 
 module.exports = {
