402bd6b8fdfa91307fe4a129ef0d4b6e509634da
dok <dumansky@gmail.com>
2019-01-28 10:59:20 +0200

    better minification, remove webrtc usage

--- src/core/handlers/vfDataHandler.js
+++ src/core/handlers/vfDataHandler.js
@@ -1,15 +1,15 @@
-import {updateInteraction} from './updateHandler';
-import WebRTC from '../webrtc/webrtc';
+// import {updateInteraction} from './updateHandler';
+// import WebRTC from '../webrtc/webrtc';
 import {vffData} from '../vffData.js';
 
 function handleVFData(data) {
     if(data.settings.sync && !window.webrtc) {
         // window.webrtc.close();
-        window.webrtc = new WebRTC(data.token, 'sync', {
-            onMessage: function (message) {
-                updateInteraction(message);
-            }
-        });
+        // window.webrtc = new WebRTC(data.token, 'sync', {
+        //     onMessage: function (message) {
+        //         updateInteraction(message);
+        //     }
+        // });
     }
     if(data.mode){
         window.vff.mode = data.mode;
